0f7d16288c7a11fc8c8f79f82d5ad7e623d39917
dupageinspect-beep <dupageinspect@gmail.com>
2026-09-19 23:26:00 -0500

    Isolate gnulib diagnostic symbol inside static archive

--- build-static-git.sh
+++ build-static-git.sh
@@ -16,8 +16,13 @@ curl --fail --location --proto '=https' https://www.kernel.org/pub/software/scm/
 printf '%s  %s\n' '7e3e6c36decbd8f1eedd14d42db6674be03671c2204864befa2a41756c5c8fc4' git.tar.xz | sha256sum -c -
 tar -xf git.tar.xz
 cd git-2.50.1
-# Namespace Git error() separately from the statically linked libidn2/gnulib symbol.
-make -j2 prefix=/opt/merge-proof-git CFLAGS='-O2 -fno-omit-frame-pointer -Derror=git_error' LDFLAGS=-static \
+# Rename only libidn2's private gnulib diagnostic symbol inside the disposable
+# static archive, preserving all of its internal references. Git's distinct
+# error() function has a different signature and must never be interposed.
+sha256sum /usr/lib/libidn2.a > "$TASK_OUTPUT/libidn2-before.txt"
+objcopy --redefine-sym error=merge_proof_idn2_error /usr/lib/libidn2.a
+sha256sum /usr/lib/libidn2.a > "$TASK_OUTPUT/libidn2-after.txt"
+make -j2 prefix=/opt/merge-proof-git CFLAGS='-O2 -fno-omit-frame-pointer' LDFLAGS=-static \
   CURL_LDFLAGS="$(pkg-config --libs --static libcurl)" NO_GETTEXT=YesPlease NO_TCLTK=YesPlease \
   NO_REGEX=NeedsStartEnd NO_PERL=YesPlease NO_PYTHON=YesPlease NO_EXPAT=YesPlease all install
 for TASK_BINARY in /opt/merge-proof-git/bin/git /opt/merge-proof-git/libexec/git-core/git-remote-https; do
