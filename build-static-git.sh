0dc9fdcd1bf0ae34a99b5a2fdec5ca4cecf5e92b
dupageinspect-beep <dupageinspect@gmail.com>
2026-09-19 23:23:11 -0500

    Namespace Git diagnostic symbol for static libidn2 linking

--- build-static-git.sh
+++ build-static-git.sh
@@ -16,7 +16,8 @@ curl --fail --location --proto '=https' https://www.kernel.org/pub/software/scm/
 printf '%s  %s\n' '7e3e6c36decbd8f1eedd14d42db6674be03671c2204864befa2a41756c5c8fc4' git.tar.xz | sha256sum -c -
 tar -xf git.tar.xz
 cd git-2.50.1
-make -j2 prefix=/opt/merge-proof-git CFLAGS='-O2 -fno-omit-frame-pointer' LDFLAGS=-static \
+# Namespace Git error() separately from the statically linked libidn2/gnulib symbol.
+make -j2 prefix=/opt/merge-proof-git CFLAGS='-O2 -fno-omit-frame-pointer -Derror=git_error' LDFLAGS=-static \
   CURL_LDFLAGS="$(pkg-config --libs --static libcurl)" NO_GETTEXT=YesPlease NO_TCLTK=YesPlease \
   NO_REGEX=NeedsStartEnd NO_PERL=YesPlease NO_PYTHON=YesPlease NO_EXPAT=YesPlease all install
 for TASK_BINARY in /opt/merge-proof-git/bin/git /opt/merge-proof-git/libexec/git-core/git-remote-https; do
